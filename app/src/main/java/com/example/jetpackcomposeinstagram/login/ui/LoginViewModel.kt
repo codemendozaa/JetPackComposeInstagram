4013ff379185214c048af4344b35be893ce5a7ba
erixjavier <Mendhoza123#>
2023-03-09 22:02:15 -0500

    Implementando capa de Dominio use case

--- app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/login/ui/LoginViewModel.kt
@@ -4,8 +4,11 @@ import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.jetpackcomposeinstagram.login.domain.LoginUseCase
 
 class LoginViewModel:ViewModel() {
+    val loginUseCase = LoginUseCase()
+
     private val _email = MutableLiveData<String>()
     val email : LiveData<String> = _email
 
